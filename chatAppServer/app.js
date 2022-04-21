c1d028e923f45e2b0fd37a937102743c8befc623
Samuel-Brr <samuel.barre972@gmail.com>
2022-04-21 21:20:51 +0200

    Intégration de pusher au backend

--- chatAppServer/app.js
+++ chatAppServer/app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const cors = require('cors')
+const Pusher = require('pusher')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -28,8 +29,41 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 //configs
 mongoose.connect(process.env.MONGODB_URI)
-  .then(() => console.log("Connected to the database !"))
-  .catch((e) => console.log("Couldn't connect to the databse", e));
+  .then((res) => {
+    console.log("Connected to the database !");
+  })
+.catch((e) => console.log("Couldn't connect to the databse", e)); 
+
+const pusher = new Pusher({
+    appId: "1399436",
+    key: "cee542e476f0a7f6c638",
+    secret: "fc475b17b783db0dbee9",
+    cluster: "eu",
+    useTLS: true
+  });
+
+const db = mongoose.connection;
+
+db.once('open', () => {
+  console.log('db connection successful');
+  const messageCollection = db.collection('messages');
+  const changeStream = messageCollection.watch();
+  changeStream.on('change', (change)=>{
+    // console.log("what is in change ", change)
+    if(change.operationType == 'insert'){
+      const messageDetails = change.fullDocument;
+      pusher.trigger(
+        'message', 'inserted', {
+          message: messageDetails.message,
+          chat_id: messageDetails.chat_id,
+          from: messageDetails.from,
+          time: messageDetails.time
+        }
+      )
+    }
+  }) 
+}) 
+ 
 
 //routing
 app.use('/', indexRouter);
