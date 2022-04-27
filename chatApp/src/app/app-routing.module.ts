a412555a0df9f3bd0d4ddbbb15d8d45c49fc48e8
Samuel-Brr <samuel.barre972@gmail.com>
2022-04-27 10:30:26 +0200

    Suppression du folder et du menu

--- chatApp/src/app/app-routing.module.ts
+++ chatApp/src/app/app-routing.module.ts
@@ -10,10 +10,6 @@ const routes: Routes = [
     pathMatch: 'full'
   },
   {
-    path: 'folder/:id',
-    loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
-  },
-  {
     path: 'home',
     loadChildren: () => import('./PAGES/home/home.module').then( m => m.HomePageModule),
     canLoad: [AuthGuard]
