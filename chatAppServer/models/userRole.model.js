bcc12887eb11e719220baf25bca44a0ed03e986e
Samuel-Brr <samuel.barre972@gmail.com>
2022-07-05 12:44:19 +0200

    Creation of the mongoose schema userRole
    To track the chats permissions over the app

---
+++ chatAppServer/models/userRole.model.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userRole = new Schema({
+    description: "Standard role for non-admin users, can only CRUD on hiw own items",
+    users: {
+        type: Schema.Types.ObjectId, ref: 'Chat'
+    }
+})
+
+module.exports = mongoose.model('userRole', userRole)
