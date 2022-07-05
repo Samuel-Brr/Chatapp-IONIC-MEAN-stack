f2da029c86d17ab27c287f327d239e0c567c2b43
Samuel-Brr <samuel.barre972@gmail.com>
2022-07-05 12:48:42 +0200

    Creation of the adminRole mongoose Schema
    To track admin users accross the app

---
+++ chatAppServer/models/adminRole.model.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const adminRole = new Schema({
+    description: "Admin role, can access admin dashboard, delete chats and messages accross the entire app",
+    users: {
+        type: Schema.Types.ObjectId, ref: 'Chat'
+    }
+})
+
+module.exports = mongoose.model('userRole', adminRole)
