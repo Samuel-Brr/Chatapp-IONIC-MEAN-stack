9c278ec42d6aa9ad31ea84d4f0bc2fed9363345f
Samuel-Brr <samuel.barre972@gmail.com>
2022-07-10 15:16:21 +0200

    update of the admin page

--- chatApp/src/app/PAGES/admin/admin.page.ts
+++ chatApp/src/app/PAGES/admin/admin.page.ts
@@ -1,4 +1,8 @@
+/* eslint-disable @typescript-eslint/member-ordering */
 import { Component, OnInit } from '@angular/core';
+import { BehaviorSubject, catchError, Observable, tap, throwError } from 'rxjs';
+import { Chat } from 'src/app/MODELS/chat.model';
+import { ApiService } from 'src/app/SERVICES/api.service';
 
 @Component({
   selector: 'app-admin',
@@ -7,9 +11,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminPage implements OnInit {
 
-  constructor() { }
+  private subject = new BehaviorSubject<any[]>([]);
+  chatArr$: Observable<any[]> = this.subject.asObservable();
+
+  constructor(private api: ApiService) { }
 
   ngOnInit() {
+    this.getAllChats();
+    console.log('All chats: ', this.chatArr$);
   }
+
+  getAllChats(){
+    this.api.getChats()
+      .pipe(
+        catchError(err => throwError(() => {
+            new Error(err);
+            alert('Unable to get list of chats');
+          })),
+        tap(chats => {
+          this.subject.next(chats);
+        })
+      )
+      .subscribe();
+    console.log(this.chatArr$);
+  }
+
+  messagesTotal(){
+    let count = 0;
+
+    this.chatArr$
+      .pipe(
+        tap(chats => {
+
+          chats.forEach(chat => count += chat.messages.length);
+
+        })
+      )
+      .subscribe();
+
+    return count;
+  }
+
 
 }
